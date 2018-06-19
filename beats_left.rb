3ee3f5ecc1a247cdb4bc056946627bfe8e48c65a
abreternitz <aliceburian@gmail.com>
2018-06-19 20:37:51 +0000

    automatically backed up by learn

--- beats_left.rb
+++ beats_left.rb
@@ -5,7 +5,7 @@ def dance
       break
     else
       puts "Slide to the left, Slide to the Right, and Kick!"
-      sleep(0.1)
+      sleep(0.05)
       beats_left_in_song=beats_left_in_song - 1
       puts "#{beats_left_in_song} beats left"
     end
