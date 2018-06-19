d8286c03b713fce279b3caf4d2e38ae8035f9f68
abreternitz <aliceburian@gmail.com>
2018-06-19 20:33:59 +0000

    automatically backed up by learn

---
+++ beats_left.rb
@@ -0,0 +1,13 @@
+def dance
+  beats_left_in_song=60
+  loop do
+    if beats_left_in_song==0  
+      break
+    else
+      puts "Slide to the left, Slide to the Right, and Kick!"
+      beats_left_in_song=beats_left_in_song-1
+    end
+    
+  end
+  
+end
